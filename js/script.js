aaf60e233523aa66b701357b19f2bd650bc8a923
stephenliu8964 <dq89_64@hotmail.com>
2023-03-09 18:32:20 +1100

    SIT725 Prac 1
    implemented the html and js files

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ function changeText() {
     document.getElementById("heading").innerHTML = textsArray[number];
 }
 
+
 function getRandomNumberBetween(min, max) {
     return Math.floor(Math.random() * (max-min+1) + min);
 }
